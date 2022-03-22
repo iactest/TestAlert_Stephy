c7236fc3071ce9baa65462645744091ed21fd0c7
poornatest457 <98936171+poornatest457@users.noreply.github.com>
2022-03-22 10:36:27 +0530

    coomit_1

---
+++ aws copy/compute/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+}
+provider "aws" {
+  profile = "default"
+  region  = "us-west-24"
+}
+resource "aws_instance" "ebs_optimized" {
+  ami           = var.ami_id
+  instance_type = var.type
+  #ebs_optimized = false
+  #monitoring = false
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
+
+resource "aws_ebs_volume" "example" {
+  availability_zone = "us-west-2a"
+  size              = 40
+  encrypted         = false
+  tags = {
+    Name = "HelloWorld"
+  }
+}
---
+++ aws_1/storage/main.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+resource "aws_s3_bucket" "mfadelete" {
+  bucket = "examplebuckettftest"
+  acl    = var.public
+  versioning {
+    mfa_delete = "false"
+    enabled = "false"
+  }
+   website {
+    index_document = "index.html"
+    error_document = "error.html"
+
+    routing_rules = <<EOF
+[{
+    "Condition": {
+        "KeyPrefixEquals": "docs/"
+    },
+    "Redirect": {
+        "ReplaceKeyPrefixWith": "documents/"
+    }
+}]
+EOF
+  }
+}
+
+resource "aws_db_instance" "education" {
+    name                      = var.name
+    instance_class            = var.class
+    allocated_storage         = var.storage
+    backup_retention_period   = -1
+    storage_encrypted         = false
+    #kms_key_id = aws_kms_key.mykey.arn
+}
